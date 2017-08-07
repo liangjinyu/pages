98bdfdd812febfa3a610ed3dd5a3c644addb01ed
Liang Jinyu <jinyu.liang@mi-me.com>
2017-08-07 18:21:33 +0800

    sidebar event test

--- ljy/offcanvas.js
+++ ljy/offcanvas.js
@@ -8,4 +8,16 @@ $(document).ready(function () {
     $("#centerMain").load("carousel.html");
  });
 
+  $("a.sidebar-link").click(function(){
+    $(".sidebar-link").filter(".active").removeClass("active");
+    $(this).addClass("active");
+    if($(this).attr("id") == "sidebar_baidu"){
+      alert("你点击了"+$(this).text()+",我要跳出去了");
+    }else{
+      alert("你点击了"+$(this).text()+",然而我并没有做什么");
+    }
+    
+ });
+
+
 });
