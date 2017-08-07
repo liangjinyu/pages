2991d8adf374ae129d213dbfc9e0ee16d058f968
Liang Jinyu <jinyu.liang@mi-me.com>
2017-08-07 10:00:05 +0800

    example test

--- ljy/offcanvas.js
+++ ljy/offcanvas.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
   $('[data-toggle="offcanvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active')
   });
+
+ $("#carouseInPage").click(function(){
+    alert("info");
+
+    $("#centerMain").empty();
+    $("#centerMain").load("carousel.html");
+ });
+
 });
